b85f051f1dc1ce47cc7c7545ab0bc7934bcbb5a8
Giordan Escalona <40776278+gescalona1@users.noreply.github.com>
2024-05-27 15:43:02 -0400

    Added periodic generator (no tests)

--- src/generator/generator.rs
+++ src/generator/generator.rs
@@ -1,19 +1,15 @@
 ///A generator is an interface that tells us that given a base set
 ///It will transform the set into another set as long as the operation is closed.
 ///Meaning that the set yielded is from same domain as the single operand.
-pub trait Generator<T> 
+pub trait Generator<'a, T> 
 where 
     T: PartialEq+Sized+Clone
 {
     fn start(&self) -> Vec<T>;
-    fn set_start(&self, start: &Vec<T>);
+    fn set_start(&mut self, start: &Vec<T>);
     /// This is supposed to manage adding some function to the generator.
-    fn add(&self, f: fn(T) -> T);
+    fn add(&mut self, f: Box<dyn Fn(T) -> T>);
     /// This clears the list of permutations.
-    fn clear(&self);
-    fn iter(&self) -> GenIter<T>;
-}
-
-pub struct GenIter<T> {
-
+    fn clear(&mut self);
+    fn iter(&'a self) -> Box<dyn Iterator<Item = Vec<T>> + 'a>;
 }
--- src/generator/periodic.rs
+++ src/generator/periodic.rs
@@ -1,4 +1,3 @@
-use crate::group::cycle::Cycle;
 use std::hash::Hash;
 use crate::generator::generator::Generator;
 
@@ -7,7 +6,7 @@ where
     T: Copy+Clone+Hash+Eq+'static
 {
     start: Vec<T>,
-    permutations: Vec<fn(T) -> T>
+    permutations: Vec<Box<dyn Fn(T) -> T>>
 }
 
 impl<T> PeriodicGen<T> 
@@ -22,29 +21,72 @@ where
     }
 }
 
-impl<T> Generator<T> for PeriodicGen<T>
+impl<'a, T> Generator<'a, T> for PeriodicGen<T>
 where
     T: Copy+Clone+Hash+Eq+'static
 {
     fn start(&self) -> Vec<T> {
-        self.start
+        self.start.clone()
     }
 
-    fn set_start(&self, start: &Vec<T>) {
-        self.start = self.start.clone()
+    fn set_start(&mut self, start: &Vec<T>) {
+        self.start = start.clone()
     }
 
-    fn add(&self, f: fn(T) -> T) {
+    fn add(&mut self, f: Box<dyn Fn(T) -> T>) {
         self.permutations.push(f);
     }
 
-    fn clear(&self) {
+    fn clear(&mut self) {
         self.permutations.clear();
     }
 
 
 
-    fn iter(&self) -> super::generator::GenIter<T> {
-        todo!()
+    fn iter(&'a self) -> Box<dyn Iterator<Item = Vec<T>> + 'a> {
+        Box::new(PeriodicGenIter::new(self))
+    }
+}
+
+pub struct PeriodicGenIter<'a, T> 
+where
+    T: Copy+Clone+Hash+Eq+'static
+{
+    curr: Vec<T>,
+    index_state: usize,
+    generator: &'a PeriodicGen<T>,
+}
+
+impl<'a, T> PeriodicGenIter<'a, T> 
+where
+    T: Copy+Clone+Hash+Eq+'static
+{
+    fn new(periodic_generator: &'a PeriodicGen<T>) -> Self {
+        PeriodicGenIter {
+            curr: periodic_generator.start(),
+            index_state: 0,
+            generator: periodic_generator
+        }
+    }
+}
+
+impl<'a, T> Iterator for PeriodicGenIter<'a, T> 
+where 
+    T: Copy+Clone+Hash+Eq+'static
+{
+    type Item = Vec<T>;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        let funcs = &self.generator.permutations;
+
+        let next_vec = self.curr
+            .iter()
+            .map(|x| funcs[self.index_state](*x))
+            .collect();
+        self.index_state = (self.index_state + 1) % (funcs.len());
+        let old = self.curr.clone();
+        self.curr = next_vec;
+
+        Some(old)
     }
 }
