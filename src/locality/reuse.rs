e7530cbc144cfc6d0b473ca978cb10e0d1ed2aa6
Giordan Escalona <40776278+gescalona1@users.noreply.github.com>
2024-06-05 10:16:49 -0400

    Added way to calculate hits^{-1}

--- src/locality/reuse.rs
+++ src/locality/reuse.rs
@@ -2,6 +2,8 @@ use std::fmt::Debug;
 use std::hash::Hash;
 use std::collections::{HashMap, HashSet};
 
+use petgraph::data;
+
 /// This will calculate the forward distance.
 /// This is done by looking ahead and refreshing the last seen index in its calculation.
 /// (similar to live analysis, where we read backwards)
@@ -31,6 +33,35 @@ where T: Clone+Eq+Hash+Debug
     reuse_distance
 }
 
+fn calculate_lru_hits<T>(trace: &Vec<T>, cache_size: usize) -> usize
+    where T: Clone+Eq+Hash+Debug
+{
+    calculate_reuse_distance(trace).into_iter()
+        .filter(|x| *x != -1)
+        .filter(|x| *x <= cache_size as i32)
+        .count()
+}
+
+fn factorial(num: i128) -> i128 {
+    (1..=num).product()
+}
+
+fn combinations(n: i128, k: i128) -> i128 {
+    (factorial(n) / factorial(k)) / factorial(n - k) 
+}
+
+fn calculate_lru_hits_formula(data_items: i128, cache_size: i128, hits: i128) -> i128 {
+    if 2 * cache_size <=  data_items + hits {
+        let m = data_items as i128;
+        let c = cache_size as i128;
+        let h = hits as i128;
+
+        factorial(c) * combinations(c, h) * combinations(m - c, c - h) * factorial(m - c)
+    } else {
+        0
+    }
+}
+
 fn calculate_dmc<T>(trace: &Vec<T>) -> f64
 where T: Clone+Eq+Hash+Debug
 {
@@ -42,8 +73,12 @@ where T: Clone+Eq+Hash+Debug
 
 #[cfg(test)]
 mod tests {
-    use crate::reuse::calculate_dmc;
-    use crate::reuse::calculate_reuse_distance;
+    use crate::locality::reuse::calculate_dmc;
+    use crate::locality::reuse::calculate_lru_hits;
+    use crate::locality::reuse::calculate_reuse_distance;
+    use crate::locality::reuse::factorial;
+
+    use super::calculate_lru_hits_formula;
     
     #[test]
     fn simple_trace() {
@@ -51,6 +86,51 @@ mod tests {
         let rd = calculate_reuse_distance(&trace);
         debug_assert_eq!(rd, vec![4, 2, 3, -1, -1, -1, -1]);
     }
+
+    #[test]
+    fn simple_trace_hits() {
+        let trace = vec!["a", "b", "c", "b", "d", "c", "a"];
+        let cache_size = 3;
+        let num_hits = calculate_lru_hits(&trace, cache_size);
+        debug_assert_eq!(num_hits, 2);
+    }
+
+    #[test]
+    fn equivalent_comb_formula_1() {
+        let data_items = 7;
+        let cache_size = 5;
+
+        let a: i128 = (0..=cache_size).into_iter()
+            .map(|h| calculate_lru_hits_formula(data_items, cache_size, h))
+            .sum();
+        
+        debug_assert_eq!(a, factorial(data_items));
+    }
+
+    #[test]
+    fn equivalent_comb_formula_2() {
+        let data_items = 19;
+        let cache_size = 2;
+
+        let a: i128 = (0..=cache_size).into_iter()
+            .map(|h| calculate_lru_hits_formula(data_items, cache_size, h))
+            .sum();
+        
+        debug_assert_eq!(a, factorial(data_items));
+    }
+
+    #[test]
+    fn equivalent_comb_formula_3() {
+        let data_items = 39;
+        let cache_size = 4;
+
+        let a: i128 = (0..=cache_size).into_iter()
+            .map(|h| calculate_lru_hits_formula(data_items, cache_size, h))
+            .sum();
+        
+        debug_assert_eq!(a, factorial(data_items));
+    }
+    
 
     #[test]
     fn cyclic() {
